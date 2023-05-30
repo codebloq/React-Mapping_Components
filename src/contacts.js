a6bbef076d0236c286c88510b1973cd5a03f4c70
The Advocate <134937174+lawyerWho@users.noreply.github.com>
2023-05-30 22:03:42 +0530

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,28 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Person One",
+    imgURL:
+      "https://images.pexels.com/photos/3483800/pexels-photo-3483800.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    phone: "+66 123-456-7890",
+    email: "abc@123.com"
+  },
+  {
+    id: 2,
+    name: "Person Two",
+    imgURL:
+      "https://images.pexels.com/photos/2693814/pexels-photo-2693814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    phone: "+66 456-123-7890",
+    email: "123@abc.com"
+  },
+  {
+    id: 3,
+    name: "Person Three",
+    imgURL:
+      "https://images.pexels.com/photos/13081260/pexels-photo-13081260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    phone: "+66 123-789-4560",
+    email: "xyz@123.com"
+  }
+];
+
+export default contacts;
